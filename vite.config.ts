9f33eb28ea4b8eeb1ddc1d5cf04f18e7f1299034
Marcin Kopanski <marcin.kopanski@gmail.com>
2023-03-20 21:19:59 +0100

    init

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import EsLint from "vite-plugin-linter";
+import tsConfigPaths from "vite-tsconfig-paths";
+
+import * as packageJson from "./package.json";
+
+const { EsLinter, linterPlugin } = EsLint;
+
+export default defineConfig((configEnv) => ({
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    linterPlugin({
+      include: ["./src}/**/*.{ts,tsx}"],
+      linters: [new EsLinter({ configEnv })],
+    }),
+    dts({
+      include: ["src/component/"],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve("src", "components/index.ts"),
+      name: "KopanskiReactLibrary",
+      formats: ["es", "umd"],
+      fileName: (format) => `kopanski-react-library.${format}.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.peerDependencies)],
+    },
+  },
+}));
